bc371cf8db767e1ae3db12c328d6b9d47af9f1b9
amanjol98 <amanjols98@gmail.com>
2026-03-05 21:01:00 +0500

    Add files via upload

---
+++ дз4/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// Задание 1
+
+	// bannerWidth := 12
+	// bannerHeight := 8
+	// bannerArea := bannerWidth * bannerHeight
+
+	// fmt.Println(bannerArea)
+
+	// halfBannerArea := bannerArea / 2
+	// fmt.Println(halfBannerArea)
+
+	// bannerBorderLength := (bannerWidth + bannerHeight) * 2
+	// fmt.Println(bannerBorderLength)
+
+	// Задание 2
+
+	// boxCount := 29
+	// couriers := 5
+	// leftoverBoxes := boxCount % couriers
+	// fmt.Println(leftoverBoxes)
+
+	// Задание 3
+
+	// tempMorning := 18
+	// tempAfternoon := 30
+	// tempEvening := 22
+
+	// totalTemp := tempMorning + tempAfternoon + tempEvening
+	// averageTemp := totalTemp / 3
+
+	// fmt.Println("Средяя температура за день =", averageTemp)
+
+	// Задание 4
+
+	// knownWords := 47
+	// wordsGoal := 120
+
+	// progressPercent := int(float64(knownWords) / float64(wordsGoal) * 100)
+
+	// fmt.Println(progressPercent, "%")
+
+	// Задание 5
+
+	// coins := 0
+	// fmt.Println(coins)
+
+	// coins += 500
+	// fmt.Println(coins)
+
+	// coins += 1200
+	// fmt.Println(coins)
+
+	// coins /= 2
+	// fmt.Println(coins)
+
+	// coins *= 2
+	// fmt.Println(coins)
+
+	// coins -= 300
+	// fmt.Println(coins)
+
+	// Задание 6
+
+	// participants := 42
+	// groupCount := 8
+	// participantsPerGroup := participants / groupCount
+	// fmt.Println(participantsPerGroup)
+
+	// fmt.Println(participants % groupCount)
+
+	// Задание 7
+
+	// fmt.Println(20 - 4*3)
+	// fmt.Println((20 - 4) * 3)
+	// Здесь правила как в математике, сначало в приоритете скобки, после умножение и деление, дальше + и -
+
+	// Задание 8
+
+	squareValue := 81
+	result := math.Sqrt(float64(squareValue))
+
+	fmt.Println(result)
+
+	multiplier := 5
+	exponent := 2
+
+	result2 := math.Pow(float64(multiplier), float64(exponent))
+
+	fmt.Println(result2)
+}
