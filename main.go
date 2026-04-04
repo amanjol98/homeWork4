754ce46dd2b64193bb09f5a6c6550e0e4883a595
amanjol <amanjols98@gmail.com>
2026-04-05 02:36:30 +0500

    fixed

--- main.go
+++ main.go
@@ -2,97 +2,96 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	// Задание 1
+	// 	// Задание 1
 
-	// bannerWidth := 12
-	// bannerHeight := 8
-	// bannerArea := bannerWidth * bannerHeight
+	// 	// bannerWidth := 12
+	// 	// bannerHeight := 8
+	// 	// bannerArea := bannerWidth * bannerHeight
 
-	// fmt.Println(bannerArea)
+	// 	// fmt.Println(bannerArea)
 
-	// halfBannerArea := bannerArea / 2
-	// fmt.Println(halfBannerArea)
+	// 	// halfBannerArea := bannerArea / 2
+	// 	// fmt.Println(halfBannerArea)
 
-	// bannerBorderLength := (bannerWidth + bannerHeight) * 2
-	// fmt.Println(bannerBorderLength)
+	// 	// bannerBorderLength := (bannerWidth + bannerHeight) * 2
+	// 	// fmt.Println(bannerBorderLength)
 
-	// Задание 2
+	// 	// Задание 2
 
-	// boxCount := 29
-	// couriers := 5
-	// leftoverBoxes := boxCount % couriers
-	// fmt.Println(leftoverBoxes)
+	// 	// boxCount := 29
+	// 	// couriers := 5
+	// 	// leftoverBoxes := boxCount % couriers
+	// 	// fmt.Println(leftoverBoxes)
 
-	// Задание 3
+	// 	// Задание 3
 
-	// tempMorning := 18
-	// tempAfternoon := 30
-	// tempEvening := 22
+	tempMorning := 18
+	tempAfternoon := 30
+	tempEvening := 22
 
-	// totalTemp := tempMorning + tempAfternoon + tempEvening
-	// averageTemp := totalTemp / 3
+	totalTemp := tempMorning + tempAfternoon + tempEvening
+	averageTemp := float64(totalTemp) / 3
 
-	// fmt.Println("Средяя температура за день =", averageTemp)
+	fmt.Printf("Средяя температура за день = %f градусов", averageTemp)
 
-	// Задание 4
+	// 	// Задание 4
 
-	// knownWords := 47
-	// wordsGoal := 120
+	// 	// knownWords := 47
+	// 	// wordsGoal := 120
 
-	// progressPercent := int(float64(knownWords) / float64(wordsGoal) * 100)
+	// 	// progressPercent := int(float64(knownWords) / float64(wordsGoal) * 100)
 
-	// fmt.Println(progressPercent, "%")
+	// 	// fmt.Println(progressPercent, "%")
 
-	// Задание 5
+	// 	// Задание 5
 
-	// coins := 0
-	// fmt.Println(coins)
+	// 	// coins := 0
+	// 	// fmt.Println(coins)
 
-	// coins += 500
-	// fmt.Println(coins)
+	// 	// coins += 500
+	// 	// fmt.Println(coins)
 
-	// coins += 1200
-	// fmt.Println(coins)
+	// 	// coins += 1200
+	// 	// fmt.Println(coins)
 
-	// coins /= 2
-	// fmt.Println(coins)
+	// 	// coins /= 2
+	// 	// fmt.Println(coins)
 
-	// coins *= 2
-	// fmt.Println(coins)
+	// 	// coins *= 2
+	// 	// fmt.Println(coins)
 
-	// coins -= 300
-	// fmt.Println(coins)
+	// 	// coins -= 300
+	// 	// fmt.Println(coins)
 
-	// Задание 6
+	// 	// Задание 6
 
-	// participants := 42
-	// groupCount := 8
-	// participantsPerGroup := participants / groupCount
-	// fmt.Println(participantsPerGroup)
+	// 	// participants := 42
+	// 	// groupCount := 8
+	// 	// participantsPerGroup := participants / groupCount
+	// 	// fmt.Println(participantsPerGroup)
 
-	// fmt.Println(participants % groupCount)
+	// 	// fmt.Println(participants % groupCount)
 
-	// Задание 7
+	// 	// Задание 7
 
-	// fmt.Println(20 - 4*3)
-	// fmt.Println((20 - 4) * 3)
-	// Здесь правила как в математике, сначало в приоритете скобки, после умножение и деление, дальше + и -
+	// 	// fmt.Println(20 - 4*3)
+	// 	// fmt.Println((20 - 4) * 3)
+	// 	// Здесь правила как в математике, сначало в приоритете скобки, после умножение и деление, дальше + и -
 
-	// Задание 8
+	// 	// Задание 8
 
-	squareValue := 81
-	result := math.Sqrt(float64(squareValue))
+	// 	squareValue := 81
+	// 	result := math.Sqrt(float64(squareValue))
 
-	fmt.Println(result)
+	// 	fmt.Println(result)
 
-	multiplier := 5
-	exponent := 2
+	// 	multiplier := 5
+	// 	exponent := 2
 
-	result2 := math.Pow(float64(multiplier), float64(exponent))
+	// 	result2 := math.Pow(float64(multiplier), float64(exponent))
 
-	fmt.Println(result2)
+	// 	fmt.Println(result2)
 }
